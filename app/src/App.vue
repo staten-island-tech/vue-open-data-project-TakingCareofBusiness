30e99e25310632b9c1577630adcf29b6e144a6a2
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-05 12:29:09 -0500

    Ive seen footage

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
       <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
         <nav>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
@@ -20,6 +16,5 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 <style scoped></style>
