32c3948265361490b18ae3db20e5755c50cdb361
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-05 12:32:56 -0500

    Raymond needs a zero on his HOS he brought the cards

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div></div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup>
+const API = 'https://data.cityofnewyork.us/resource/7479-ugqb.json'
+</script>
+
+<style lang="scss" scoped></style>
