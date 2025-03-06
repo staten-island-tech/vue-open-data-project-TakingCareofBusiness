1fe5d285f644515976c01bf24fa823d1d3c9c4e3
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-06 11:48:01 -0500

    Mark is pocket watching

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -4,6 +4,12 @@
 
 <script setup>
 const API = 'https://data.cityofnewyork.us/resource/7479-ugqb.json'
+async function getData(URL) {
+  try {
+    const response = await fetch(URL)
+    const info = await response.json()
+  } catch (error) {}
+}
 </script>
 
 <style lang="scss" scoped></style>
