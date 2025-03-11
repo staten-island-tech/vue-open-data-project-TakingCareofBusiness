4bb2a1c53d393864afb5300350b830ac1691584a
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-11 12:25:24 -0400

    sorry for all the commits

--- app/src/components/TestChart.vue
+++ app/src/components/TestChart.vue
@@ -8,43 +8,32 @@
 import { ref, onMounted } from 'vue'
 import Chart from 'chart.js'
 
-export default {
-  setup() {
-    const myChartRef = ref(null) // Create a reference for the canvas element
+const myChartRef = ref(null) // Create a reference for the canvas element
 
-    // Initialize chart after the component is mounted
-    onMounted(() => {
-      const ctx = myChartRef.value
+onMounted(() => {
+  const ctx = myChartRef.value
 
-      new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [
-            {
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              borderWidth: 1,
-            },
-          ],
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
         },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
-      })
-    })
-
-    return {
-      myChartRef,
-    }
-  },
-}
+      },
+    },
+  })
+})
 </script>
 
-<style scoped>
-/* Add any additional styling here */
-</style>
+<style></style>
