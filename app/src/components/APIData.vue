54eae190c443ed5fb7a71fbab1e5499ff51390ff
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-24 08:39:01 -0400

    I need help

--- app/src/components/APIData.vue
+++ app/src/components/APIData.vue
@@ -5,15 +5,18 @@
 <script setup>
 import { onMounted } from 'vue'
 const API = 'https://data.cityofnewyork.us/resource/7479-ugqb.json'
+let dat = ''
 onMounted(async () => {
   try {
     const response = await fetch(API)
     const info = await response.json()
+    let dat = info
     console.log(info)
   } catch (error) {
     console.log(error)
   }
 })
+export let data = dat
 </script>
 
 <style lang="scss" scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div>
     <APIData />
     <div class="flex flex-wrap gap-4 mt-4 justify-center">
-      <ScreenCards v-for="item in list" :key="item.name" :item="item" class="flex-none w-1/6">
+      <ScreenCards v-for="item in data" :key="item.name" :item="item" class="flex-none w-1/6">
       </ScreenCards>
     </div>
   </div>
@@ -10,6 +10,7 @@
 
 <script setup>
 import APIData from '../components/APIData.vue'
+import { data } from '../components/APIData.vue'
 import ScreenCards from '../components/ScreenCards.vue'
 </script>
 
