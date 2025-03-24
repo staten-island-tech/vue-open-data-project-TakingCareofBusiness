d139904f93fd25e003a0adb4cdad8acdb8c6b488
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-23 22:51:09 -0400

    Whatever

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div><h1 class="text-3xl">Eyad</h1></div>
     <APIData />
+    <div class="flex flex-wrap gap-4 mt-4 justify-center">
+      <ScreenCards v-for="item in " :key="item.name" :item="item" class="flex-none w-1/6">
+      </ScreenCards>
+    </div>
   </div>
 </template>
 
 <script setup>
 import APIData from '../components/APIData.vue'
+import ScreenCards from '../components/ScreenCards.vue'
 </script>
 
 <style lang="scss" scoped></style>
