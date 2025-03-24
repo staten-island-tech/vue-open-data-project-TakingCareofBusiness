1c9bef0d6ab6e374b68a663ae9c62ea9c7820ac4
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-03-23 22:52:59 -0400

    CRAP

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div>
     <APIData />
     <div class="flex flex-wrap gap-4 mt-4 justify-center">
-      <ScreenCards v-for="item in " :key="item.name" :item="item" class="flex-none w-1/6">
+      <ScreenCards v-for="item in list" :key="item.name" :item="item" class="flex-none w-1/6">
       </ScreenCards>
     </div>
   </div>
